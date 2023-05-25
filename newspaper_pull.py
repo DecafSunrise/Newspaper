e42b1201055fbee9aebc26c7616bc16e0ef0887a
decafsunrise <danny.cardella@gmail.com>
2023-05-25 17:31:37 -0400

    remove underperforming URLs

--- newspaper_pull.py
+++ newspaper_pull.py
@@ -5,11 +5,85 @@ start = time.time()
 import newspaper
 from newspaper import news_pool
 
+print("""
+ _   _                                               
+| \ | |                                              
+|  \| | _____      _____ _ __   __ _ _ __   ___ _ __ 
+| . ` |/ _ \ \ /\ / / __| '_ \ / _` | '_ \ / _ \ '__|
+| |\  |  __/\ V  V /\__ \ |_) | (_| | |_) |  __/ |   
+\_| \_/\___| \_/\_/ |___/ .__/ \__,_| .__/ \___|_|   
+                        | |         | |              
+                        |_|         |_|              """)
+
+
 ## Load the sites from sites.py (in the same dir)
-from sites import urls
+# from sites import urls
+
+urls = [
+'http://www.huffingtonpost.com',
+ 'http://cnn.com',
+ 'http://www.time.com',
+ 'http://www.cnbc.com',
+ 'http://www.pcmag.com',
+ 'http://www.foxnews.com',
+ 'http://theatlantic.com',
+ 'http://www.bbc.co.uk',
+ 'http://www.vice.com',
+ 'http://espn.com',
+ 'http://www.npr.org'
+"https://www.buzzfeednews.com/",
+"https://www.thedailybeast.com/",
+"https://www.motherjones.com/",
+"https://www.msnbc.com/",
+"https://www.newyorker.com/",
+"https://www.nytimes.com/",
+"https://slate.com/",
+"https://www.vox.com/",
+"https://abcnews.go.com/",
+"https://apnews.com/",
+"https://www.bloomberg.com/",
+"https://www.cbsnews.com/",
+"https://www.theguardian.com/us",
+"https://www.insider.com/",
+"https://www.nbcnews.com/",
+"https://www.politico.com/",
+"https://www.propublica.org/",
+"https://www.washingtonpost.com/",
+"https://www.usatoday.com/",
+"https://news.yahoo.com/",
+"https://www.axios.com/",
+"https://www.csmonitor.com/",
+"https://www.forbes.com/",
+"https://www.marketwatch.com/",
+"https://www.newsnationnow.com/",
+"https://www.newsweek.com/",
+"https://www.reuters.com/",
+"https://thehill.com/",
+"https://www.wsj.com/",
+"https://thedispatch.com/",
+"https://www.theepochtimes.com/",
+"https://ijr.com/",
+"https://nypost.com/",
+"https://thepostmillennial.com/",
+"https://reason.com/",
+"https://www.washingtontimes.com/",
+"https://spectator.org/",
+"https://www.breitbart.com/",
+"https://www.theblaze.com/",
+"https://www1.cbn.com/",
+"https://dailycaller.com/",
+"https://www.dailymail.co.uk/ushome/index.html",
+"https://www.dailywire.com/",
+"https://thefederalist.com/",
+"https://www.nationalreview.com/",
+"https://www.newsmax.com/",
+"https://freebeacon.com/",
+"https://www.oann.com/",
+ ]
+
 
 # saveDir = "/mnt/nfs/newspaperPull"
-saveDir = r"D:\Work\Data\Newspaper"
+saveDir = r"/home/dan/articles"
 
 start = datetime.datetime.now()
 print(f"Started at: {start.strftime('%m/%d/%Y, %H:%M:%S')}")
